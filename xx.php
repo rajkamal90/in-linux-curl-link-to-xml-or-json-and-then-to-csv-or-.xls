1d43868e987092b6ae83c0207c9469893c782f2d
rajkamal90 <rajkamalsharma90@gmail.com>
2014-09-02 11:51:40 +0530

    Create xx.php

---
+++ xx.php
@@ -0,0 +1,34 @@
+//paste your curl link in the terminal
+
+//followed by >> file  name /../path to save the xml or json file
+
+//run the script below
+//after giving the script path .
+//>> write the file name example xxx.csv
+
+
+
+
+
+<?php
+$parser=xml_parser_create();
+
+function char($parser,$data)
+  {
+  echo $data.',';
+  }
+
+xml_set_character_data_handler($parser,"char");
+$fp=fopen("....path/xxx.xml","r");// path to your file
+echo ',custid,issued,valid,span,offset,ptu,F50,F10,F90';
+while ($data=fread($fp,4096))
+  {
+  xml_parse($parser,$data,feof($fp)) or
+  die (sprintf("XML Error: %s at line %d",
+  xml_error_string(xml_get_error_code($parser)),
+  xml_get_current_line_number($parser)));
+
+  }
+
+xml_parser_free($parser);
+?> 
